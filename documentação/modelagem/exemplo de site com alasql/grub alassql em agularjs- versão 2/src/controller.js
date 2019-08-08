830cd628f274db897dbd279dcebdc3016dfaeb0c
Kevin Vieira Guimarães <guimaraesprogramador@gmail.com>
2019-08-08 16:47:48 -0300

    feito o banco de dados

--- documentação/modelagem/exemplo de site com alasql/grub alassql em agularjs- versão 2/src/controller.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("grub",["servico"])
-app.controller("programa2",function($scope,servicos){
-    $scope.listar = function(){
-        servicos.select();
-    }
-    $scope.insert = function(){
-        servicos.adiconar();
-    }
-});
