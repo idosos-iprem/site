23aff36bb0b6be88e7f80340eca907a1bc01a460
Kevin Vieira Guimarães <guimaraesprogramador@gmail.com>
2019-09-04 16:55:11 -0300

    ver ainda trafdico de netware

--- documentação/modelagem/exemplo de site com alasql/grub alassql em agularjs/www/teste de automatizacao/src/selenium.js
+++
@@ -1,9 +0,0 @@
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const firefox = require('selenium-webdriver/firefox');
-
-var caminho = __dirname;
-let driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    .usingServer(caminho)
-    .build();
---
+++ documentação/modelagem/exemplo de site com alasql/grub alassql em agularjs/www/teste de automatizacao/src/teste.py
@@ -0,0 +1,24 @@
+
+class tempo_resposta_banco:
+
+    def Primeiro_modulo_primeiro(self):
+        from urllib.response import urlparse
+        text = "http://transparencia.portalfacil.com.br/api/servidores?type=json&idCliente=214&page=1&pageSize=100&dtcompetencia=01/2019";
+       # string = "http://127.0.0.1:8080/Primeiro_modulo_primeiro.json";
+        import  selenium.webdriver as driver;
+        drive = driver.Firefox();
+        drive.get(text);
+class selenium_firefox_aplicativo:
+
+    def aplicacao(self):
+        import  selenium.webdriver as driver;
+        Firefox_text = driver.Firefox().get("file:///C:/Users/kevin.guimaraes/Desktop/site/documenta%C3%A7%C3%A3o/modelagem/exemplo%20de%20site%20com%20alasql/grub%20alassql%20em%20agularjs/www/index.html");
+
+# resposta do servido
+teste = tempo_resposta_banco();
+teste.Primeiro_modulo_primeiro();
+# print(valor);
+# nome = "kevin"
+# senha = "12345"
+# servido = selenium_firefox_aplicativo()
+# servido.aplicacao();
